cd47560568214a36dcca9c7587547020deef9594
HenriTeinturier <henri.teinturier@gmail.com>
2022-03-31 12:25:24 +0200

    transformation de notre compoosant en class pour pouvoir utiliser un objet state en faisant appel a React.component via notre constructeur et extends de la class

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // Data
 import currenciesList from 'src/data/currencies';
 
@@ -15,30 +17,74 @@ const amountToConvert = 1;
 const currencyToConvert = 'Unidted States Dollar';
 
 // == Composant
-function Converter() {
-  // permet de  faire affichage conditionnel
-  // ici isOpen pour le composant Currencies
-  let isOpen = true;
+// function Converter() {
+//   // permet de  faire affichage conditionnel
+//   // ici isOpen pour le composant Currencies
+//   let isOpen = false;
 
-  const handleClick = () => {
-    if (isOpen) { isOpen = false; console.log('false') } else { isOpen = true; console.log('true') }
-  };
+//   const handleClick = () => {
+//     if (isOpen) { isOpen = false; console.log('false') } else { isOpen = true; console.log('true') }
+//   };
 
-  return (
-    <div className="converter">
-      <Header
-        amountToConvert={amountToConvert}
-        currency="euro"
-      />
-      <button type="button" onClick={handleClick}>Afficher / Cacher les devises</button>
-      {isOpen && <Currencies currencies={currenciesList} />}
-      <Amount
-        amountToConvert={amountToConvert}
-        rate={1.69}
-        currency={currencyToConvert}
-      />
-    </div>
-  );
+//   return (
+//     <div className="converter">
+//       <Header
+//         amountToConvert={amountToConvert}
+//         currency="euro"
+//       />
+//       <button type="button" onClick={handleClick}>Afficher / Cacher les devises</button>
+//       {isOpen && <Currencies currencies={currenciesList} />}
+//       <Amount
+//         amountToConvert={amountToConvert}
+//         rate={1.69}
+//         currency={currencyToConvert}
+//       />
+//     </div>
+//   );
+// }
+
+
+//* Transformation en class
+//* extends React.component + ajouter un render pour la partie JSX
+// == Composant
+class Converter extends React.Component {
+  // Pour mettre en place un state :
+  // - On converti le composant en classe qui étend de React.Component
+  // (ne pas oublier l'import de React)
+  // - Il faut mettre en place un constructeur dans lequel on initialise les props
+  // du composant ET SURTOUT le state !!
+  // => Le state est un objet qui va contenir des propriétés représentant l'état de notre UI
+  constructor(props) {
+    // On execute le constructeur de la classe parente pour profiter de ses fonctionnalités
+    // Si on a des props, on les initialise à ce moment
+    super(props);
+    // On crée notre state ici
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  handleClick() {
+    if (this.isOpen) { this.isOpen = false; console.log('false') } else { isOpen = true; console.log('true') }
+  }
+
+  render() {
+    return (
+      <div className="converter">
+        <Header
+          amountToConvert={amountToConvert}
+          currency="euro"
+        />
+        <button type="button" onClick={this.handleClick}>Afficher / Cacher les devises</button>
+        {this.isOpen && <Currencies currencies={currenciesList} />}
+        <Amount
+          amountToConvert={amountToConvert}
+          rate={1.69}
+          currency={currencyToConvert}
+        />
+      </div>
+    );
+  }
 }
 
 // == Export
